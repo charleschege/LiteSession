ba54cb5e43ecc33f5e455a47a62ad9ab608eacfa
Charles Chege <charleschege@protonmail.ch>
2021-02-15 10:37:10 +0300

    add errors for bad tokens

--- src/errors.rs
+++ src/errors.rs
@@ -1,8 +1,11 @@
-enum LiteSessionErrors {
+#[derive(Debug)]
+pub enum LiteSessionError {
     NonceTooShort,
     NonceTooLong,
     ServerKeyTooShort,
     ServerKeyTooLong,
     ChaCha8DecryptionError,
     ChaCha8EncryptionError,
+    TokenSizeTooLarge,
+    TokenFieldsError,
 }
