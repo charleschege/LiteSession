137ad0ad0a77bdf70d1667154ec0c57d4e32b411
Charles Chege <charleschege@protonmail.ch>
2021-02-15 18:09:47 +0300

    Add destructure methods

--- src/data.rs
+++ src/data.rs
@@ -1,14 +1,14 @@
-use crate::Role;
+use crate::{LiteSessionError, Role};
 
 #[derive(Debug)]
-pub struct LiteSessionData<T> {
+pub struct LiteSessionData {
     username: String,
     role: Role,
     tag: Option<String>,
-    acl: Vec<T>,
+    acl: Vec<String>, //FIXME Make `acl` an enum of well known ACL fields
 }
 
-impl<T> Default for LiteSessionData<T> {
+impl Default for LiteSessionData {
     fn default() -> Self {
         Self {
             username: String::default(),
@@ -19,10 +19,7 @@ impl<T> Default for LiteSessionData<T> {
     }
 }
 
-impl<T> LiteSessionData<T>
-where
-    T: core::fmt::Display + core::fmt::Debug + core::cmp::Ord,
-{
+impl LiteSessionData {
     pub fn username(&mut self, value: &str) -> &mut Self {
         self.username = value.into();
 
@@ -41,13 +38,13 @@ where
         self
     }
 
-    pub fn add_acl(&mut self, resourse: T) -> &mut Self {
+    pub fn add_acl(&mut self, resourse: &str) -> &mut Self {
         self.acl.push(resourse.into());
 
         self
     }
 
-    pub fn remove_acl(&mut self, resource: T) -> Option<&mut Self> {
+    pub fn remove_acl(&mut self, resource: &str) -> Option<&mut Self> {
         match self.acl.binary_search(&resource.into()) {
             Ok(index) => {
                 self.acl.remove(index);
@@ -59,29 +56,57 @@ where
 
     pub fn build(&self) -> String {
         let mut acl_token = String::default();
-        let ls_separator = '⥂';
-        let acl_separator = '⇅';
         let mut acl_list = String::default();
 
         acl_token.push_str(&self.username);
-        acl_token.push(ls_separator);
+        acl_token.push(self.ls_separator());
         acl_token.push_str(&Role::to_string(&self.role));
-        acl_token.push(ls_separator);
+        acl_token.push(self.ls_separator());
 
         match &self.tag {
-            None => (),
+            None => acl_token.push_str("None"),
             Some(tag) => acl_token.push_str(&tag),
         }
 
         let initial = &self.acl[0];
         acl_list.push_str(&format!("{:?}", initial));
         self.acl.iter().skip(1).for_each(|item| {
-            acl_list.push(acl_separator);
+            acl_list.push(self.acl_separator());
             acl_list.push_str(&format!("{:?}", item))
         });
-        acl_token.push(ls_separator);
+        acl_token.push(self.ls_separator());
         acl_token.push_str(&acl_list);
 
         acl_token
+    }
+
+    pub fn destructure(mut self, data: &str) -> Result<Self, LiteSessionError> {
+        let first_split: Vec<&str> = data.split(self.ls_separator()).collect();
+        if first_split.len() != 4_usize {
+            return Err(LiteSessionError::DataFieldsLengthError);
+        }
+
+        self.username = first_split[0].into();
+        self.role = Role::from_str(first_split[1]);
+        self.tag = match first_split[2] {
+            "None" => None,
+            _ => Some(first_split[2].into()),
+        };
+
+        let mut acl_list: Vec<String> = Vec::new();
+        first_split[3]
+            .split(self.acl_separator())
+            .for_each(|acl| acl_list.push(acl.into()));
+        self.acl = acl_list;
+
+        Ok(self)
+    }
+
+    fn ls_separator(&self) -> char {
+        '⥂'
+    }
+
+    fn acl_separator(&self) -> char {
+        '⇅'
     }
 }
